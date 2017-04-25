a6e8efc452b13a9f946f4112ca48bab55dbc3371
Wadson Fleurigene <wadsonf@gmail.com>
2017-04-25 19:41:52 -0400

    mongo db connector

--- bin/seeds.js
+++ bin/seeds.js
@@ -17,5 +17,23 @@ const studentsInfo [
     age: 28,
     schoolId: '1337',
     picture: '/images/blah.gif'
+  },
+  {
+    firstName: 'Nizar',
+    lastName: 'Khakdhsih',
+    age: 21,
+    schoolId: '7331',
+    picture: '/images/blah.gif'
   }
 ];
+
+// Creating a connection
+Student.create(studentsInfo, (err, studentsDoc) => {
+  if (err) {
+    throw err;
+  }
+  // For each student we add, let me know in the console, bro.
+  studentsDocs.forEarch((oneStudent) => {
+    console.log(`NEW STUDENT ${oneStudent.firstName} -> ${oneStudent._id}`);
+  });
+});
